b04b81ea26501b2666cdebb92057b9e07c0e40b1
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-18 19:24:16 +0000

    Quiz 1

---
+++ app/controllers/quiz_controller.rb
@@ -0,0 +1,5 @@
+class QuizController < ApplicationController
+
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quiz::Application.routes.draw do
+
+  root 'quiz#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
